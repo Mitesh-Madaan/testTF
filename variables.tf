723e9f7f5f4ae84e02a6dc06e37757cf6448385f
Mitesh Madaan <mitesh.madaan@nutanix.com>
2022-09-06 13:57:26 +0530

    added variables file

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "file1Name" {
+	type = string
+	description = "Name of env's file1"
+}
+
+variable "file1Content" {
+	type = string
+	description = "Content of env's file1"
+}
+
+variable "file2Name" {
+	type = string
+	description = "Name of env's file2"
+}
+
+variable "file2Content" {
+	type = string
+	description = "Content of env's file2"
+}
