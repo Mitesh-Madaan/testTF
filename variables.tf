03f348c32b251b9b8f96fb14065b550314e6b1fa
Mitesh Madaan <mitesh.madaan@nutanix.com>
2022-09-06 14:14:35 +0530

    added default variable values

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "file1Name" {
-	type = string
-	description = "test1.txt"
+        type = string
+        description = "Name of env's file1"
+	default = "file1.txt"
 }
 
 variable "file1Content" {
-	type = string
-	description = "This is test file1\n"
+        type = string
+        description = "Content of env's file1"
+	default = "test default content file1"
 }
 
 variable "file2Name" {
-	type = string
-	description = "test2.txt"
+        type = string
+        description = "Name of env's file2"
+	default = "file2.txt"
 }
 
 variable "file2Content" {
-	type = string
-	description = "This is test file2\n"
+        type = string
+        description = "Content of env's file2"
+	default = "test default content file2"
 }
