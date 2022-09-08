df61966f8d3372167b4578572532e044bcd5a511
mitesh.madaan <mitesh.madaan@nutanix.com>
2022-09-08 16:43:08 +0530

    CLI based execution

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+
+  cloud {
+    organization = "NTNX-stratos"
+
+    workspaces {
+      name = "testTF"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 resource "local_file" "File1" {
 filename = var.file1Name
 content = var.file1Content
--- var.tfvars
+++ var.tfvars
@@ -1,5 +1,2 @@
 file1Name = "file1.txt"
-file1Content = "test not default content file1\n"
-
-file2Name = "file2.txt"
-file2Content = "test not default content file2\n"
+file1Content = "test from CLI to VCS content file1\n"
